fee9c7183cc4056655f7bfaddd916db63ee4ca06
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-06 21:39:23 +0000

    enough shenanigans for espeak_Initialize

---
+++ poc.c
@@ -0,0 +1,40 @@
+#pragma leco tool
+#pragma leco add_include_dir "."
+#pragma leco add_include_dir "espeak-ng/src/include"
+#pragma leco add_include_dir "espeak-ng/src/ucd-tools/src/include"
+
+#define PATH_ESPEAK_DATA "/tmp/"
+
+#include <espeak-ng/speak_lib.h>
+
+int main() {
+  espeak_AUDIO_OUTPUT output = AUDIO_OUTPUT_SYNCH_PLAYBACK;
+  espeak_Initialize(output, 500, NULL, 0);
+}
+
+#include "espeak-ng/src/libespeak-ng/common.c"
+#include "espeak-ng/src/libespeak-ng/mnemonics.c"
+#include "espeak-ng/src/libespeak-ng/langopts.c"
+#include "espeak-ng/src/libespeak-ng/compiledict.c"
+#include "espeak-ng/src/libespeak-ng/dictionary.c"
+#include "espeak-ng/src/libespeak-ng/encoding.c"
+#include "espeak-ng/src/libespeak-ng/error.c"
+#include "espeak-ng/src/libespeak-ng/espeak_api.c"
+#include "espeak-ng/src/libespeak-ng/intonation.c"
+#include "espeak-ng/src/libespeak-ng/numbers.c"
+#include "espeak-ng/src/libespeak-ng/phonemelist.c"
+#include "espeak-ng/src/libespeak-ng/readclause.c"
+#include "espeak-ng/src/libespeak-ng/setlengths.c"
+#include "espeak-ng/src/libespeak-ng/soundicon.c"
+#include "espeak-ng/src/libespeak-ng/speech.c"
+#include "espeak-ng/src/libespeak-ng/ssml.c"
+#include "espeak-ng/src/libespeak-ng/synthdata.c"
+#include "espeak-ng/src/libespeak-ng/synthesize.c"
+#include "espeak-ng/src/libespeak-ng/tr_languages.c"
+#include "espeak-ng/src/libespeak-ng/translate.c"
+#include "espeak-ng/src/libespeak-ng/translateword.c"
+#include "espeak-ng/src/libespeak-ng/voices.c"
+#include "espeak-ng/src/libespeak-ng/wavegen.c"
+#include "espeak-ng/src/ucd-tools/src/case.c"
+#include "espeak-ng/src/ucd-tools/src/categories.c"
+#include "espeak-ng/src/ucd-tools/src/proplist.c"
